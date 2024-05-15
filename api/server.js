65f9555d70a5c877f177b9160902628c538e44f0
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2024-05-15 15:15:27 +0800

    change db to event

--- api/server.js
+++ api/server.js
@@ -6,10 +6,10 @@ const server = jsonServer.create()
 // Uncomment to allow write operations
 const fs = require('fs')
 const path = require('path')
-const filePath = path.join('db.json')
+const filePath = path.join('event.json')
 const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data);
-const router = jsonServer.router(db)
+const event = JSON.parse(data);
+const router = jsonServer.router(event)
 
 // Comment out to allow write operations
 // const router = jsonServer.router('db.json')
